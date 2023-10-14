1948b38cdd49a5b35508203dd3e34a3e690e68a8
Chrizt14k <124142032+Chrizt14k@users.noreply.github.com>
2023-10-14 15:02:57 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Chapter 2 - Exercise 2.py
@@ -0,0 +1,8 @@
+#Write a Python program to get the Python version you are using.
+
+# The import is use for the statement to bring the sys module
+import sys
+
+# Using the sys module to access system-specific parameters and functions
+# Print the Python version
+print("Python version: ", sys.version)
