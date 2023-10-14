207250be45d3a640b4521513ca691b49b203a033
Chrizt14k <124142032+Chrizt14k@users.noreply.github.com>
2023-10-14 14:53:18 +0400

    Add files via upload

---
+++ Assessment 1/Chapter 1- Getting Started with Python/Exercises/Chapter 1 - Exercise 1.py
@@ -0,0 +1,16 @@
+# Print the first line
+print("Twinkle, twinkle, little star,")
+# Print the second line 
+print("How I wonder what you are! \n")
+# The \n is the newline character I used it to separate the lines
+
+# Print the third line
+print("Up above the world so high,")
+# Print the fourth line
+print("Like a diamond in the sky.\n")
+# I used the \n again to separate the lines 
+
+# Print the fifth line
+print("Twinkle, twinkle, little star,")
+# Print the sixth line
+print("How I wonder what you are")
